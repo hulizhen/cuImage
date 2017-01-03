5172c866440a2e801eb1f83a80bd6d5fea2e70b1
Hu Lizhen <hulizhen.public@gmail.com>
2017-01-03 20:38:00 +0800

    Initial Commit

---
+++ cuImage/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  cuImage
+//
+//  Created by HuLizhen on 03/01/2017.
+//  Copyright © 2017 HuLizhen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
