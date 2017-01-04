2bcd6866322ddb65bed4cb76efa47d75706af6b0
Hu Lizhen <hulizhen.public@gmail.com>
2017-01-04 13:26:14 +0800

    Add some handy computed properties for enum SupportedHost

--- cuImage/Host.swift
+++ cuImage/Host.swift
@@ -16,3 +16,31 @@ protocol HostDelegate: class {
     func host(_ host: Host, isUploadingImageWithPercent percent: Float);
     func host(_ host: Host, didUploadImageWithURLString urlString: String);
 }
+
+enum SupportedHost: String {
+    case qiniu = "Qiniu"
+    
+    // It seems there is currently no elegant way to enumerate all the cases.
+    // So, type each cases twice!
+    static var allCases: [SupportedHost] {
+        return [.qiniu]
+    }
+    
+    private func viewInNibNamed(_ name: String) -> NSView? {
+        var views = NSArray()
+        let loaded = Bundle.main.loadNibNamed(name, owner: nil, topLevelObjects: &views)
+        return loaded ? views.firstObject as? NSView : nil
+    }
+    
+    var image: NSImage {
+        switch self {
+        case .qiniu: return #imageLiteral(resourceName: "QiniuHost")
+        }
+    }
+    
+    var view: NSView {
+        switch self {
+        case .qiniu: return viewInNibNamed("QiniuHostPreferencesView")!
+        }
+    }
+}
