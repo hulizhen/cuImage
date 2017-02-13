40f9d9ed90e1ca2b54bac87636478e9c96e316ed
Lizhen Hu <hulizhen.public@gmail.com>
2017-02-13 16:32:23 +0800

    Add services menu

---
+++ cuImage/ServicesProvider.swift
@@ -0,0 +1,16 @@
+//
+//  ServicesProvider.swift
+//  cuImage
+//
+//  Created by Lizhen Hu on 13/02/2017.
+//  Copyright © 2017 Lizhen Hu. All rights reserved.
+//
+
+import Cocoa
+
+final class ServicesProvider: NSObject {
+    func handleUploadService(_ pboard: NSPasteboard, userData: String, error: AutoreleasingUnsafeMutablePointer<NSString?>) {
+        
+    }
+}
+
